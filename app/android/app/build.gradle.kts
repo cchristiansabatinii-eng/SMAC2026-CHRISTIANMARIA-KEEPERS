ff89aefdd7793f3e9db98b29f27f59cf73c7677b
cchristiansabatinii-eng <cchristian.sabatinii@gmail.com>
2026-09-01 15:04:06 +0400

    fix: compile Android against SDK 37

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -6,7 +6,8 @@ plugins {
 
 android {
     namespace = "app.keepers.keepers"
-    compileSdk = flutter.compileSdkVersion
+    // flutter_secure_storage requires Android API 37 at compile time.
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
